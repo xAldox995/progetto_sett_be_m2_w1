709e1125fcfc3e97568bc68d0f07f49715940756
xAldox995 <valzanialdo@gmail.com>
2024-10-25 10:28:29 +0200

    Initial commit

---
+++ src/main/java/aldovalzani/progetto_sett_be_m2_w1/ProgettoSettBeM2W1Application.java
@@ -0,0 +1,13 @@
+package aldovalzani.progetto_sett_be_m2_w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettBeM2W1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettBeM2W1Application.class, args);
+	}
+
+}
---
+++ src/test/java/aldovalzani/progetto_sett_be_m2_w1/ProgettoSettBeM2W1ApplicationTests.java
@@ -0,0 +1,13 @@
+package aldovalzani.progetto_sett_be_m2_w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettBeM2W1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
