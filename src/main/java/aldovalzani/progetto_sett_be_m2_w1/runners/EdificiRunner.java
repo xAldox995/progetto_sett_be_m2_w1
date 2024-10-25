9b79320c8a6408c32e5617b92ad5f33e4a977801
xAldox995 <valzanialdo@gmail.com>
2024-10-25 13:15:16 +0200

    test sulle funzionalità EdificioService finiti

--- src/main/java/aldovalzani/progetto_sett_be_m2_w1/runners/EdificiRunner.java
+++ src/main/java/aldovalzani/progetto_sett_be_m2_w1/runners/EdificiRunner.java
@@ -1,6 +1,7 @@
 package aldovalzani.progetto_sett_be_m2_w1.runners;
 
 import aldovalzani.progetto_sett_be_m2_w1.entities.Edificio;
+import aldovalzani.progetto_sett_be_m2_w1.exceptions.NotFoundException;
 import aldovalzani.progetto_sett_be_m2_w1.services.EdificioService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,21 @@ public class EdificiRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Edificio edificio1 = new Edificio("Milano", "Via Roma 1", "Sede Centrale");
+        /*Edificio edificio1 = new Edificio("Milano", "Via Roma 1", "Sede Centrale");
         Edificio edificio2 = new Edificio("Valenzano", "Via Bari 1", "Sede Secondaria");
         edificioService.saveEdificio(edificio1);
-        edificioService.saveEdificio(edificio2);
+        edificioService.saveEdificio(edificio2);*/
+
         log.info("Edifici Inseriti: ");
         edificioService.findAllEdifici().forEach(edificio -> log.info(edificio.toString()));
+
+
+        try {
+            Edificio edificioFound = edificioService.findEdificioById(102);
+            log.info(edificioFound.toString());
+        } catch (NotFoundException ex) {
+            log.error(ex.getMessage());
+        }
 
 
     }
