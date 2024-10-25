eb391c1e41eaceafeaaad77f58cd1ff1a4cdd141
xAldox995 <valzanialdo@gmail.com>
2024-10-25 13:07:36 +0200

    inizializzione dei runner con i test dei vari service

---
+++ src/main/java/aldovalzani/progetto_sett_be_m2_w1/runners/EdificiRunner.java
@@ -0,0 +1,28 @@
+package aldovalzani.progetto_sett_be_m2_w1.runners;
+
+import aldovalzani.progetto_sett_be_m2_w1.entities.Edificio;
+import aldovalzani.progetto_sett_be_m2_w1.services.EdificioService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class EdificiRunner implements CommandLineRunner {
+
+    @Autowired
+    private EdificioService edificioService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Edificio edificio1 = new Edificio("Milano", "Via Roma 1", "Sede Centrale");
+        Edificio edificio2 = new Edificio("Valenzano", "Via Bari 1", "Sede Secondaria");
+        edificioService.saveEdificio(edificio1);
+        edificioService.saveEdificio(edificio2);
+        log.info("Edifici Inseriti: ");
+        edificioService.findAllEdifici().forEach(edificio -> log.info(edificio.toString()));
+
+
+    }
+}
